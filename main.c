3f112528a5f4f06283d339981bb9f8e440b0c91b
rababyoucode <110168436+rababyoucode@users.noreply.github.com>
2022-08-08 09:03:36 +0100

    factoriel

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fonFact (int n){
+       int f=1,i;
+       for(i=1; i<n+1; i++){
+        f=f*i;
+       }
+    return f;
+    }
+int main()
+{
+    int n ,i,f=1;
+    printf("entrer votre factorel \n");
+    scanf("%d",&n);
+
+    for(i=1; i <=n; i++){
+    f=f*i;
+    }
+    printf("le resultat est : %d \n",n,f);
+    printf("le resultat par la fonction est : %d \n",fonFact(n));
+    return 0;
+}
+
+
